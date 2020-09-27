000ce0bdfa0cd92e796f9d4593a961c7b9d55b71
WOLFGAMING18 <71986452+WOLFGAMING18@users.noreply.github.com>
2020-09-27 22:57:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 //copyright © Angry Black 2020.//❦Angry Black❦
 
 const Discord = require("discord.js");
-const myid = ['Your ID'];//هنا بتحط الايدى بتاعك بتاع الديسكورد
+const myid = ['759290068464304128'];//هنا بتحط الايدى بتاعك بتاع الديسكورد
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
